ceeac5a88e8386decfea37fd5a510c41c837b38d
nguyentrongdoan <nguyendoan206@gmail.com>
2020-07-19 21:34:50 +0700

    giao dien login moi lan 2, thay doi validator

--- src/app/register-user/register-user.component.ts
+++ src/app/register-user/register-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-register-user',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-user.component.scss']
 })
 export class RegisterUserComponent implements OnInit {
-
+  userInfor = {
+    username: '',
+    email: '',
+    password: '',
+    rememberMe: false
+  }
+  usernamePattern = /^[a-z]{6,32}$/i;
+  emailPattern = /^[a-z][a-z0-9_\.]{5,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}$/i;
+  passwordPattern = /^(?=.*[!@#$%^&*]+)[a-z0-9!@#$%^&*]{6,32}$/;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSubmit1(form: NgForm) {
+    console.log(form);
+  }
+  onSubmit2(form: NgForm) {
+    console.log(form);
   }
 
 }
